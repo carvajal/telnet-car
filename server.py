3981be3417c7c937484e78a90734fa218f99d906
Ernesto Carvajal <ernestocl@google.com>
2019-09-01 14:07:19 -0700

    Refactor into server class

--- server.py
+++ server.py
@@ -9,48 +9,56 @@ from _thread import start_new_thread
 HOST = '' # Symbolic name meaning all available interfaces.
 PORT = 5555 # Arbitrary non-privileged port.
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
+class Server:
 
-# BIND socket to local host and port.
-try:
-  s.bind((HOST, PORT))
-except socket.error as msg:
-  print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-  sys.exit()
-  
-print('Socket bind complete')
+  def __init__(self):
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
 
-# Start listening on socket.
-s.listen(10)
-print('Socket now listening')
+  def bind(self):
+    # BIND socket to local host and port.
+    try:
+      self.socket.bind((HOST, PORT))
+    except socket.error as msg:
+      print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+      sys.exit()      
+    print('Socket bind complete')
 
-# Function for handling connections. This will be used to create threads.
-def clientthread(conn):
-  # Sending message to connected client.
-  conn.send(b'Welcome to the server. Type something and hit enter\n')
-  
-  # Infinite loop so that function do not terminate and thread do not end..
-  while True:
+  def listen(self):
+    # Start listening on socket.
+    self.socket.listen(10)
+    print('Socket now listening')
+
+    # Now keep talking with the client.
+    while True:
+        # Wait to accept a connection - blocking call.
+      conn, addr = self.socket.accept()
+      print('Connected with ' + addr[0] + ':' + str(addr[1]))
+      
+      # Start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function..
+      start_new_thread(self._clientthread , (conn,))
+
+  # Function for handling connections. This will be used to create threads.
+  def _clientthread(self, conn):
+    # Sending message to connected client.
+    conn.send(b'Welcome to the server. Type something and hit enter\n')
     
-    # Receiving from client.
-    data = conn.recv(1024)
-    reply = b'OK...' + data
-    if not data: 
-      break
+    # Infinite loop so that function do not terminate and thread do not end..
+    while True:
+      # Receiving from client.
+      data = conn.recv(1024)
+      if not data:
+        break
+      reply = b'OK...' + data
+      conn.sendall(reply)
   
-    conn.sendall(reply)
-  
-  # Came out of loop.
-  conn.close()
+    # Came out of loop.
+    conn.close()
 
-# Now keep talking with the client.
-while True:
-    # Wait to accept a connection - blocking call.
-  conn, addr = s.accept()
-  print('Connected with ' + addr[0] + ':' + str(addr[1]))
-  
-  # Start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function..
-  start_new_thread(clientthread , (conn,))
+  def close(self):
+    self.socket.close()
 
-s.close()
+if __name__ == '__main__':
+  server = Server()
+  server.bind()
+  server.listen()
