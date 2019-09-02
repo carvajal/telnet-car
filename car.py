bbd17d1d6ad8d0d8c8d44af7e80072ca6c1da4bf
Ernesto Carvajal <ernestocl@google.com>
2019-09-01 17:28:47 -0700

    Add car with observing capabilities

---
+++ car.py
@@ -0,0 +1,70 @@
+'''
+  Thread base class that simulates a running car
+'''
+from threading import Timer
+from time import time
+
+CAR_MASS = 1500
+FORWARD_FORCE = lambda throttle_percentage: 5400 * throttle_percentage / 100.0
+DRAG_FORCE = lambda velocity: 0.2 * (velocity ** 2)
+UPDATE_INTERVAL = 0.1 # secs
+OBSERVE_INTERVAL = 1.0 # secs
+
+class Car:
+
+  def __init__(self):
+    self.velocity = 0.0
+    self.throttle_percentage = 0.0
+    self.last_updated_time = None
+    self.attached_observer = False
+
+  def get_velocity(self):
+    return self.velocity
+
+  def set_throttle_percentage(self, throttle_percentage):
+    if throttle_percentage < 0 or throttle_percentage > 100:
+      raise Exception('Throttle percentage out of bound: %f' % throttle_percentage)
+    self.throttle_percentage = throttle_percentage
+
+  def update_velocity(self):
+    if not self.last_updated_time:
+      raise Exception("start() must be called before updating velocity")
+
+    current_time = time()
+    delta_time = current_time - self.last_updated_time
+    velocity_km_h = self.velocity * 3.6
+
+    current_force = FORWARD_FORCE(self.throttle_percentage) - DRAG_FORCE(velocity_km_h)
+    current_acceleration = current_force / CAR_MASS
+
+    self.velocity += current_acceleration * delta_time
+    self.last_updated_time = current_time
+
+  def update_and_reschedule(self):
+    self.update_velocity()
+    self.schedule_next_update()
+
+  def schedule_next_update(self):
+    Timer(UPDATE_INTERVAL, self.update_and_reschedule).start()
+
+  def status(self):
+    return 'Acceleration: %.2f%% - Velocity: %.2f Km/h' % (self.throttle_percentage, self.velocity * 3.6) 
+
+  def observe_and_reschedule(self):
+    print(self.status())
+    self.schedule_next_observe()
+
+  def schedule_next_observe(self):
+    Timer(OBSERVE_INTERVAL, self.observe_and_reschedule).start()
+
+  def attach_observer(self):
+    if self.attached_observer:
+      return
+    self.attached_observer = True
+    self.schedule_next_observe();
+
+  def start(self):
+    if self.last_updated_time:
+      raise Exception('Can only call car.start() once')
+    self.last_updated_time = time()
+    self.schedule_next_update()
--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 import socket
 import sys
 from _thread import start_new_thread
+from car import Car
 
 HOST = '' # Symbolic name meaning all available interfaces.
 PORT = 5555 # Arbitrary non-privileged port.
@@ -14,6 +15,8 @@ class Server:
   def __init__(self):
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Socket created')
+    self.car = Car()
+    self.car.start()
 
   def bind(self):
     # BIND socket to local host and port.
@@ -29,6 +32,10 @@ class Server:
     self.socket.listen(10)
     print('Socket now listening')
 
+    # Start printing out the car status.
+    print('Car engine is ON')
+    self.car.attach_observer()
+
     # Now keep talking with the client.
     while True:
         # Wait to accept a connection - blocking call.
@@ -41,19 +48,40 @@ class Server:
   # Function for handling connections. This will be used to create threads.
   def _clientthread(self, conn):
     # Sending message to connected client.
-    conn.send(b'Welcome to the server. Type something and hit enter\n')
+    conn.send(b'Connection with the car established.\n')
+    conn.send(b'To check car velocity and gas throttle type:\n')
+    conn.send(b'STATUS\n')
+    conn.send(b'To press the gas pedal to 85% type:\n')
+    conn.send(b'THROTTLE 85\n')
     
-    # Infinite loop so that function do not terminate and thread do not end..
+    # Infinite loop that will finish on client disconnect.
     while True:
       # Receiving from client.
       data = conn.recv(1024)
       if not data:
         break
-      reply = b'OK...' + data
-      conn.sendall(reply)
+
+      tokens = data.split()
+      if data == b'\n':
+        reply = ''
+      elif len(tokens) == 1 and tokens[0] == b'STATUS':
+        reply = self.car.status() + '\n'
+      elif len(tokens) == 2 and tokens[0] == b'THROTTLE' and self.is_percentage(tokens[1]):
+        self.car.set_throttle_percentage(float(tokens[1]))
+        reply = 'OK\n'
+      else:
+        reply = 'Command malformed - ignored\n'
+
+      conn.sendall(reply.encode('utf-8'))
   
     # Came out of loop.
     conn.close()
+
+  def is_percentage(self, bstr):
+    if not bstr.decode('utf-8').replace('.','',1).isdigit():
+      return False
+    percentage = float(bstr.decode('utf-8'))
+    return percentage >= 0 and percentage <= 100
 
   def close(self):
     self.socket.close()
